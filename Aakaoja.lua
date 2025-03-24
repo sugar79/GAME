f7bea246d6deb9771bad2fec137a5310733b8620
𝐒𝐔𝐆𝐀𝐑 <roggps9@gmail.com>
2025-03-24 20:49:22 +0700

    Fpsdrop.lua

--- Aakaoja.lua
+++ Aakaoja.lua
@@ -1,29 +1,12 @@
-local ScreenGui = Instance.new("ScreenGui")
-local ImageLabel = Instance.new("ImageLabel")
-local CloseButton = Instance.new("TextButton")
+local function ReduceFPS()
+    RunService:Set3dRenderingEnabled(true)
+    local oldStep = RunService.RenderStepped:Connect(function()
+        task.wait(1/10)
+    end)
+    return oldStep
+end
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
--- ตั้งค่า ImageLabel (ภาพเต็มหน้าจอ)
-ImageLabel.Parent = ScreenGui
-ImageLabel.Size = UDim2.new(1, 0, 1, 0)
-ImageLabel.Position = UDim2.new(0, 0, 0, 0)
-ImageLabel.Image = "rbxassetid://111311683582193"
-ImageLabel.BackgroundTransparency = 1
-ImageLabel.ScaleType = Enum.ScaleType.Crop
-
-
-CloseButton.Parent = ScreenGui
-CloseButton.Size = UDim2.new(0, 50, 0, 50)
-CloseButton.Position = UDim2.new(1, -60, 0, 10)
-CloseButton.Text = "X"
-CloseButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-CloseButton.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
-CloseButton.Font = Enum.Font.SourceSansBold
-CloseButton.TextSize = 30
-CloseButton.BorderSizePixel = 0
-
--- กดปุ่มแล้วปิด UI
-CloseButton.MouseButton1Click:Connect(function()
-    ScreenGui:Destroy() -- ปิด UI ทิ้งไปเลย
-end)
+local function RestoreFPS(connection)
+    if connection then connection:Disconnect() end
+    RunService:Set3dRenderingEnabled(true)
+end
